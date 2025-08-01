55f4b88fb2eadb0b98737a9752de74663a917831
shannu1995 <sivapurapu95@gmail.com>
2025-08-01 17:36:29 +1000

    Button to navigate back home.

--- ResultsScreen.tsx
+++ ResultsScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import RenderHtml from 'react-native-render-html';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from './types';
@@ -8,21 +8,30 @@ type ResultsScreenProps = NativeStackScreenProps<RootStackParamList, 'ResultsScr
 
 const ResultsScreen = ({route, navigation }: ResultsScreenProps) => {
     const {matches, correctAnswers} = route.params;
+    const swappedMatches = Object.fromEntries(
+        Object.entries(matches).map(([key, value]) => [value, key])
+    );
     return (
         <View>
             <Text>Results Screen</Text>
             <View>
                 <Text>Matches:</Text>
-                {Object.entries(matches).map(([city, country]) => (
+                {Object.entries(swappedMatches).map(([city, country]) => (
                     <Text key={city}>{city} - {country}</Text>
                 ))}
             </View>
             <View>
                 <Text>Correct Answers:</Text>
-                {Object.entries(correctAnswers).map(([country, city]) => (
+                {Object.entries(correctAnswers).map(([city, country]) => (
                     <Text key={city}>{city} - {country}</Text>
                 ))}
             </View>
+        <View>
+            <Button
+                title="Back to Home"
+                onPress={() => navigation.navigate('Home')}
+            />
+        </View>
         </View>
     );
 };
